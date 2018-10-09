b16c3e5cab83d9ac91b6dfb08fb502eefb404925
gitgut0110 <jsimms0110@gmail.com>
2018-10-10 00:03:15 +0200

    apiKey migrated to config.js

--- app/backend.js
+++ app/backend.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 
+
 // Function prints Return on investment
 function rateOfReturn(arrStart, arrEnd) {
   let ror = arrEnd[4] - arrStart[4];
@@ -44,11 +45,13 @@ function dailyDrawdown(arr) {
 maxDDOutput = `
 Maximium Drawdown: ${maxDD.drawdown}% (${maxDD.peak} on ${maxDD.pDate} -> ${maxDD.trough} on ${maxDD.tDate})
 `
+firstThreeDDOutput = `First 3 Drawdowns: 
+${ddArr[0].drawdown}% (${ddArr[0].peak} on ${ddArr[0].pDate} -> ${ddArr[0].trough} on ${ddArr[0].tDate})
+${ddArr[1].drawdown}% (${ddArr[1].peak} on ${ddArr[1].pDate} -> ${ddArr[1].trough} on ${ddArr[1].tDate})
+${ddArr[2].drawdown}% (${ddArr[2].peak} on ${ddArr[2].pDate} -> ${ddArr[2].trough} on ${ddArr[2].tDate})
+`
   console.log(maxDDOutput);
-  console.log(`
-  First 3 Daily Drawdowns: `,
-  ddArr.slice(0, 3));
-  console.log('')
+  console.log(firstThreeDDOutput);
 }
 
 // Print stock prices for time range
@@ -60,15 +63,16 @@ function printEOD(arr) {
 
 // Function requests price data using variables provided by user
 module.exports = function Lillium(stock, dayInit, dayEnd, key) {
+const apiUrl = `https://www.quandl.com/api/v3/datasets/WIKI/${stock}.json?start_date=${dayInit}&end_date=${dayEnd}&order=asc&api_key=${key}`
   axios
     .get(
-      `https://www.quandl.com/api/v3/datasets/WIKI/${stock}.json?start_date=${dayInit}&end_date=${dayEnd}&order=asc&api_key=${key}`
+      apiUrl
     )
     .then(function(response) {
       const data = response.data.dataset.data;
       if (data.length === 0)
         console.log(
-          `Source has no price data for this time range, check header for available time range`
+          `Source has no price data for this time range try before 2018-03-27`
         );
       if (data.length > 0) {
         console.log(`-------------Begin Data for ${stock}------------`);
--- app/index.js
+++ app/index.js
@@ -1,21 +1,22 @@
-const Lillium = require('./backend')
+const lillium = require('./backend')
+const config = require("./config");
 
 // USER INPUT - ADJUST EQUATION INPUTS
 
 // Stock Ticker
 const ticker = [`aapl`]
-// Initial Date YYYY-MM-DD
-const startDate = `2018-01-01`
-// End Date YYYY-MM_DD   ** EMPTY DATA SET will result if Quandl has no information.
-const endDate = `2018-01-05`
-// API KEY
-const apiKey = "xGVHzXL81RZT89Jvks6a"
+// Initial Date YYYY-MM-DD  **Must be before 2018-03-27 when using Quandl
+const startDate = `2018-03-01`
+// End Date YYYY-MM_DD  **Data Only Available up to 2018-03-27
+const endDate = `2018-10-25`
+// API KEY **Create a config.js file that exports the key or simply enter below
+const apiKey = config.QuandlKey
 
 
 console.log(`Lillium Calculator initiated for stock(s): ${ticker} between ${startDate} - ${endDate} on key:${apiKey}` )
 
 // Lillium(ticker,startDate,endDate,apiKey)
-ticker.forEach(stock => Lillium(stock,startDate,endDate,apiKey))
+ticker.forEach(stock => lillium(stock,startDate,endDate,apiKey))
 
 
 function sum(a, b) {
